29b1406bcbf9eb4d22211f768f6629e4ced647e2
Nathaniel Brion Sison <ngsison@outlook.com>
2024-11-09 14:46:46 +0800

    Prepare sample responses

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/translate', (req, res) => {
+  res.send('Some text translated from OCR');
+});
+
+app.get('/check', (req, res) => {
+  const body = {
+    aiGenerated: true
+  }
+  res.send(body);
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
